f270c00a3a8a535675aaa73befe5de7404903c43
Kevin Stone <ruca6616@gmail.com>
2023-02-07 16:50:41 -0500

    practiced templating/variables

--- index.php
+++ index.php
@@ -15,7 +15,17 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // define a default route (328/week6)
-$f3->route('GET /', function () {
+$f3->route('GET /', function ($f3) {
+
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'my title');
+
+    // add your fav food and color
+    $f3->set('food', 'oysters');
+    $f3->set('color', 'black');
+
+    $f3->set('radius', 10);
 
     // instantiate a view
     $view = new Template();
