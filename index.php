c182159262ff981c7b9117b7d60feeda5809d4bf
Kevin Stone <ruca6616@gmail.com>
2023-02-08 19:54:23 -0500

    templating and condition practice

--- index.php
+++ index.php
@@ -24,8 +24,27 @@ $f3->route('GET /', function ($f3) {
     // add your fav food and color
     $f3->set('food', 'oysters');
     $f3->set('color', 'black');
-
     $f3->set('radius', 10);
+
+    $fruits = array("apple", "banana", "orange");
+    $f3->set('fruits', $fruits);
+
+
+    $colors = array("red", "blue", "yellow");
+    $f3->set('colors', $colors);
+
+
+    $cupcakes = array("chocolate"=>"Chocolate Ganache",
+        "strawberry"=>"Strawberry");
+    $f3->set('cupcakes', $cupcakes);
+
+    // add a variable that stores your age
+    $f3->set('age', 25);
+
+
+    // in the view page, if age is over 30, print
+    // "You are mature". Otherwise, print "You are a sprint chicken"
+
 
     // instantiate a view
     $view = new Template();
